189784dd526482fae8ece94c43ac68a8f3d5fea0
André Neves <aneves.sw@gmail.com>
2025-04-11 00:38:08 +0100

    Highlight the selected score.

--- src/app/scores/list/list.component.ts
+++ src/app/scores/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Score, ScoreService } from '../scores.service';
 
 @Component({
   selector: 'app-list',
   imports: [
     RouterLink,
+    RouterLinkActive,
     RouterOutlet,
   ],
   templateUrl: './list.component.html',
