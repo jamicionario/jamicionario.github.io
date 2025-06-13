63a9a3a4ab1cdab8dc477c4aa2818ea1d129c127
André Neves <aneves.sw@gmail.com>
2025-06-13 22:39:43 +0100

    Update about page and licensing information.

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import sourceInfo from '@public/source-info.json';
+import { RouterLink } from '@angular/router';
+import { SourceInfo } from '@models/source-info';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    RouterLink,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  readonly sourceInfo: SourceInfo = sourceInfo;
 
+  get contributors(): string {
+    return describeContributors(this.sourceInfo.source.contributors);
+  }
+
+  get dataContributors(): string {
+    return describeContributors(this.sourceInfo.data.contributors);
+  }
+}
+
+export function describeContributors(contributors: string[], cuttofLength: number = 3): string {
+  if (contributors.length == 0) {
+    return 'the community';
+  }
+  if (contributors.length <= cuttofLength) {
+    return contributors.join(", ");
+  }
+  return contributors.slice(0, 3).join(", ") + `, and ${contributors.length - cuttofLength} more`;
 }
