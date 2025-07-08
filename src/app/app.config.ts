d74091ebfb0f3353a3594fe98915a960b4f397d8
André Neves <aneves.sw@gmail.com>
2025-07-08 21:21:26 +0100

    Fix navigation to fragments.
    Example: navigating to /download#files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,16 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({
+      // These two values allow us to navigate to fragments. Example: /download#files
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+    })),
     provideClientHydration(withEventReplay()),
     // TODO: extend TitleStrategy to include score name in title.
   ]
